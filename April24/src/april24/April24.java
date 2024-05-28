120c777d4395572d3be6f422363daf188f50eb21
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2024-05-29 03:42:54 +0600

    add

---
+++ April24/src/april24/April24.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package april24;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author USERAS
+ */
+public class April24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the year: ");
+//        int year = input.nextInt();
+//        if((year %4 == 0 && year %100 != 0) || year %400 == 0){
+//            System.out.println(year+" is a leap year");
+//        }
+//        else{
+//            System.out.println(year+" is not a leap year");
+//        }
+
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter day number of week: ");
+//        int dayNumber = input.nextInt();
+//        String  day = " ";
+//        switch (dayNumber) {
+//            case 1:
+//                day = "Saturday";
+//                break;
+//            case 2:
+//                day = "Sunday";
+//            case 3:
+//                day = "Monday";
+//                break;
+//            case 4:
+//                day = "Tuesday";
+//                break;
+//            case 5:
+//                day = "Wednesday";
+//                break;
+//            case 6:
+//                day = "Thursday";
+//                break;
+//            case 7:
+//                day = "Friday";
+//                break;
+//            default:
+//                day = " invalid";
+//        }
+//        System.out.println("It is "+day);
+    }
+    
+}

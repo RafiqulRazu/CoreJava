5913a4208ed3236d11d1b12d33e35b4ccb6dab86
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2024-05-29 03:35:49 +0600

    Add files via upload
    trwyrwhjw

---
+++ April23/src/april23/April23.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package april23;
+
+import java.util.Scanner;
+
+
+
+
+
+/**
+ *
+ * @author USERAS
+ */
+public class April23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Celsius: ");
+//        float celsius = input.nextFloat();
+//        float fahrenheit = ((9*celsius)+160)/5;
+//        System.out.println(fahrenheit);
+
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("1st number: ");
+//        int num1 = input.nextInt();
+//        System.out.println("2nd number: ");
+//        int num2 = input.nextInt();
+//        System.out.println("3rd number: ");
+//        int num3 = input.nextInt();
+//        int maxnum;
+//        if(num1>num2 && num1>num3){
+//            maxnum = num1;
+//        }
+//        else if(num2>num1 && num2>num3){
+//            maxnum = num2;
+//        }
+//        else{
+//            maxnum = num3;
+//        }
+//        System.out.println("Maximum number is "+maxnum);
+
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the mark: ");
+//        int mark = input.nextInt();
+//        if(mark>100 || mark<0){
+//            System.out.println("Invalid mark");
+//            return;
+//        }
+//        String grade;
+//        if(mark<33){
+//            grade ="F";
+//        }
+//        else if(mark<40){
+//            grade ="D";
+//        }
+//        else if(mark<50){
+//            grade ="C";
+//        }
+//        else if(mark<60){
+//            grade ="B";
+//        }
+//        else if(mark<70){
+//            grade ="A-";
+//        }
+//        else if(mark<80){
+//            grade ="A";
+//        }
+//        else{
+//            grade ="A+";
+//        }
+//        System.out.println("Your grade is "+grade);
+
+
+
+
+        
+    }
+    
+}

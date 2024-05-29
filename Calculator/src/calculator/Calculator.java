6fbc7046086f7259391800017c8f73de85daf787
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2024-05-29 07:04:22 +0600

    add

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USERAS
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int secondNumber = sc.nextInt();
+
+        System.out.print("Enter the type of operation you want to perform (+, -, *, /, %): ");
+        String operation = sc.next();
+        int result = performOperation(firstNumber, secondNumber, operation);
+        System.out.println("Your answer is: " + result);
+    }
+
+    public static int performOperation(int firstNumber, int secondNumber, String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "%":
+                result = firstNumber % secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+        }
+        return result;
+    }
+
+}
